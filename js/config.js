465ac7164e785a308f66536f3edf0d66d1e8867d
FlipOutDerrimut <derrimutmanager@flipout.com.au>
2026-09-11 16:45:41 +1000

    Change SUPABASE_ANON_KEY in config.js
    Updated SUPABASE_ANON_KEY to a new value.

--- js/config.js
+++ js/config.js
@@ -4,4 +4,4 @@
 // SUPABASE_ANON_KEY is the long "anon public" key (NOT the service_role key)
 
 const SUPABASE_URL = "https://njtatshrdoldwpbvitjz.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qdGF0c2hyZG9sZHdwYnZpdGp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxMDM3ODQsImV4cCI6MjEwNDY3OTc4NH0.IdsHHjeq9ncUqfjcWp9u-fsHoyY5SNsXE0bkO2t8tjg";
+const SUPABASE_ANON_KEY = "sb_publishable_8IRxWnDjg_r3EWQtuGPnqw_3-hxRwJ9";
